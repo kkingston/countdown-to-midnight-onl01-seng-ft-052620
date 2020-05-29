f52cef959d179b2824a611f69beadc8296e245df
Kayla Kingston <kkingston09@gmail.com>
2020-05-29 21:41:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,12 +5,11 @@ def countdown(n)
    while n > 0 
      puts "#{n} SECOND(S)!"
      n -= 1
-     sleep 5
   end
   "HAPPY NEW YEAR!"
 end 
 
-def countdown(n)
+def countdown_with_sleep(n)
 
    while n > 0 
      puts "#{n} SECOND(S)!"
